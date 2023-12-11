06be8e9f97734d491662ec6863305461ee1c6457
mkindiykpi@gmail.com <mkindiykpi@gmail.com>
2023-12-11 17:54:20 +0200

    test

---
+++ ElectDiscipline_Web/Services/DisciplineNumberService.cs
@@ -0,0 +1,72 @@
+﻿using ElectDiscipline_Utility;
+using ElectDiscipline_Web.Models;
+using ElectDiscipline_Web.Models.Dto;
+using ElectDiscipline_Web.Services.IServices;
+
+namespace ElectDiscipline_Web.Services
+{
+    public class DisciplineNumberService : BaseService, IDisciplineNumberService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string disciplineUrl;
+
+        public DisciplineNumberService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            disciplineUrl = configuration.GetValue<string>("ServiceUrls:DisciplineAPI");
+
+        }
+
+        public Task<T> CreateAsync<T>(DisciplineNumberCreateDTO dto, string token)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = dto,
+                Url = disciplineUrl + "/api/v1/disciplineAPI",
+                //Token = token
+            });
+        }
+
+        public Task<T> DeleteAsync<T>(int id, string token)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = disciplineUrl + "/api/v1/disciplineAPI/" + id,
+                //Token = token
+            });
+        }
+
+        public Task<T> GetAllAsync<T>(string token)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = disciplineUrl + "/api/v1/disciplineAPI",
+                //Token = token
+            });
+        }
+
+        public Task<T> GetAsync<T>(int id, string token)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = disciplineUrl + "/api/v1/disciplineAPI/" + id,
+                //Token = token
+            });
+        }
+
+        public Task<T> UpdateAsync<T>(DisciplineNumberUpdateDTO dto, string token)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = dto,
+                Url = disciplineUrl + "/api/v1/disciplineAPI/" + dto.Id,
+                //Token = token
+            });
+        }
+    }
+}
--- ElectDiscipline_Web/Services/IServices/IDisciplineNumberService.cs
+++ ElectDiscipline_Web/Services/IServices/IDisciplineNumberService.cs
@@ -6,8 +6,8 @@ namespace ElectDiscipline_Web.Services.IServices
     {
         Task<T> GetAllAsync<T>(string token);
         Task<T> GetAsync<T>(int id, string token);
-        Task<T> CreateAsync<T>(DisciplineCreateDTO dto, string token);
-        Task<T> UpdateAsync<T>(DisciplineUpdateDTO dto, string token);
+        Task<T> CreateAsync<T>(DisciplineNumberCreateDTO dto, string token);
+        Task<T> UpdateAsync<T>(DisciplineNumberUpdateDTO dto, string token);
         Task<T> DeleteAsync<T>(int id, string token);
     }
 }
